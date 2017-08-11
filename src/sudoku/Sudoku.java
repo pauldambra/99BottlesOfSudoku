bd75adfd401a04c345c5a6f49f139af0c10bed80
Ian Haworth <ian.haworth@thehutgroup.com>
2017-08-11 14:20:24 +0100

    Sudoku Kata from XP Man on 2017/08/10

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -3,7 +3,27 @@ package sudoku;
 public class Sudoku {
 
     public int[][] solve(int[][] puzzle) {
-        return null;
+        for(int row = 0; row < 2 ; row ++) {
+            for(int col = 0 ; col < 2 ; col ++) {
+                if (puzzle[row][col] == 0) {
+                    for (int i = 1; i <= 2; i++) {
+                        if (puzzle[row][otherIndex(col)] == i || puzzle[otherIndex(row)][col] == i) {
+                            puzzle[row][col] = otherValue(i);
+                            return solve(puzzle);
+                        }
+                    }
+                }
+            }
+        }
+        return puzzle;
+    }
+
+    private int otherIndex(int index) {
+        return 1-index;
+    }
+
+    private int otherValue(int i) {
+        return 3 - i;
     }
 
 }
