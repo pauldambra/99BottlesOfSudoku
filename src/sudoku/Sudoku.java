426c92cae6da5b6de091836a0991175302e1ee82
pauldambra <pauldambra@gmail.com>
2017-08-13 15:10:47 +0100

    shameless green

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -3,7 +3,52 @@ package sudoku;
 public class Sudoku {
 
     public int[][] solve(int[][] puzzle) {
-        return null;
+
+        if (puzzle[1][1] == 0) {
+            if (puzzle[1][0] == 2 || puzzle[0][1] == 2) {
+                puzzle[1][1] = 1;
+                return solve(puzzle);
+            }
+            if (puzzle[1][0] == 1 || puzzle[0][1] == 1){
+                puzzle[1][1] = 2;
+                return solve(puzzle);
+            }
+        }
+
+        if (puzzle[1][0] == 0) {
+            if (puzzle[1][1] == 2 || puzzle[0][0] == 2) {
+                puzzle[1][0] = 1;
+                return solve(puzzle);
+            }
+            if (puzzle[1][1] == 1 || puzzle[0][0] == 1) {
+                puzzle[1][0] = 2;
+                return solve(puzzle);
+            }
+        }
+
+        if (puzzle[0][1] == 0) {
+            if (puzzle[0][0] == 2 || puzzle[1][1] == 2) {
+                puzzle[0][1] = 1;
+                return solve(puzzle);
+            }
+            if (puzzle[0][0] == 1 || puzzle[1][1] == 1){
+                puzzle[0][1] = 2;
+                return solve(puzzle);
+            }
+        }
+
+        if (puzzle[0][0] == 0) {
+            if (puzzle[0][1] == 2 || puzzle[1][0] == 2) {
+                puzzle[0][0] = 1;
+                return solve(puzzle);
+            }
+            if (puzzle[0][1] == 1 || puzzle[1][0] == 1) {
+                puzzle[0][0] = 2;
+                return solve(puzzle);
+            }
+        }
+
+        return puzzle;
     }
 
 }
--- test/sudoku/Sudoku2By2Test.java
+++ test/sudoku/Sudoku2By2Test.java
@@ -17,96 +17,96 @@ public class Sudoku2By2Test extends SudokuTest {
                 dynamicTest("Solving an already solved puzzle makes no changes", () -> sudokuTest(
                         "1 2",  "1 2",
                         "2 1",  "2 1"))
-//                ,dynamicTest("Solving the other already solved puzzle makes no changes", () -> sudokuTest(
-//                        "2 1",  "2 1",
-//                        "1 2",  "1 2"))
-//
-                // One cell missing
-//                ,dynamicTest("Top Left Missing", () -> sudokuTest(
-//                        "0 2",  "1 2",
-//                        "2 1",  "2 1"))
-//                ,dynamicTest("Top Left Missing, other solution", () -> sudokuTest(
-//                        "0 1",  "2 1",
-//                        "1 2",  "1 2"))
-//
-//                ,dynamicTest("Top Right Missing", () -> sudokuTest(
-//                        "1 0",  "1 2",
-//                        "2 1",  "2 1"))
-//                ,dynamicTest("Top Right Missing, other solution", () -> sudokuTest(
-//                        "2 0",  "2 1",
-//                        "1 2",  "1 2"))
-//
-//                ,dynamicTest("Bottom Right Missing", () -> sudokuTest(
-//                        "1 2",  "1 2",
-//                        "2 0",  "2 1"))
-//                ,dynamicTest("Bottom Right Missing, other solution", () -> sudokuTest(
-//                        "2 1",  "2 1",
-//                        "1 0",  "1 2"))
-//
-//                ,dynamicTest("Bottom Left Missing", () -> sudokuTest(
-//                        "1 2",  "1 2",
-//                        "0 1",  "2 1"))
-//                ,dynamicTest("Bottom Left Missing, other solution", () -> sudokuTest(
-//                        "2 1",  "2 1",
-//                        "0 2",  "1 2"))
-//
-                // Two cells missing
-//                ,dynamicTest("Top Row Missing", () -> sudokuTest(
-//                        "0 0",  "1 2",
-//                        "2 1",  "2 1"))
-//                ,dynamicTest("Top Row Missing, other solution", () -> sudokuTest(
-//                        "0 0",  "2 1",
-//                        "1 2",  "1 2"))
-//
-//                ,dynamicTest("Right Column Missing", () -> sudokuTest(
-//                        "1 0",  "1 2",
-//                        "2 0",  "2 1"))
-//                ,dynamicTest("Right Column Missing, other solution", () -> sudokuTest(
-//                        "2 0",  "2 1",
-//                        "1 0",  "1 2"))
-//
-//                ,dynamicTest("Bottom Row Missing", () -> sudokuTest(
-//                        "1 2",  "1 2",
-//                        "0 0",  "2 1"))
-//                ,dynamicTest("Bottom Row Missing, other solution", () -> sudokuTest(
-//                        "2 1",  "2 1",
-//                        "0 0",  "1 2"))
-//
-//                ,dynamicTest("Left Column Missing", () -> sudokuTest(
-//                        "0 2",  "1 2",
-//                        "0 1",  "2 1"))
-//                ,dynamicTest("Left Column Missing, other solution", () -> sudokuTest(
-//                        "0 1",  "2 1",
-//                        "0 2",  "1 2"))
-//
-                // Three cells missing
-//                ,dynamicTest("Top Left only present", () -> sudokuTest(
-//                        "1 0",  "1 2",
-//                        "0 0",  "2 1"))
-//                ,dynamicTest("Top Left only present, other solution", () -> sudokuTest(
-//                        "2 0",  "2 1",
-//                        "0 0",  "1 2"))
-//
-//                ,dynamicTest("Top Right only present", () -> sudokuTest(
-//                        "0 2",  "1 2",
-//                        "0 0",  "2 1"))
-//                ,dynamicTest("Top Right only present, other solution", () -> sudokuTest(
-//                        "0 1",  "2 1",
-//                        "0 0",  "1 2"))
-//
-//                ,dynamicTest("Bottom Right only present", () -> sudokuTest(
-//                        "0 0",  "1 2",
-//                        "0 1",  "2 1"))
-//                ,dynamicTest("Bottom Right only present, other solution", () -> sudokuTest(
-//                        "0 0",  "2 1",
-//                        "0 2",  "1 2"))
-//
-//                ,dynamicTest("Bottom Left only present", () -> sudokuTest(
-//                        "0 0",  "1 2",
-//                        "2 0",  "2 1"))
-//                ,dynamicTest("Bottom Left only present, other solution", () -> sudokuTest(
-//                        "0 0",  "2 1",
-//                        "1 0",  "1 2"))
+                ,dynamicTest("Solving the other already solved puzzle makes no changes", () -> sudokuTest(
+                        "2 1",  "2 1",
+                        "1 2",  "1 2"))
+
+                 //One cell missing
+                ,dynamicTest("Top Left Missing", () -> sudokuTest(
+                        "0 2",  "1 2",
+                        "2 1",  "2 1"))
+                ,dynamicTest("Top Left Missing, other solution", () -> sudokuTest(
+                        "0 1",  "2 1",
+                        "1 2",  "1 2"))
+
+                ,dynamicTest("Top Right Missing", () -> sudokuTest(
+                        "1 0",  "1 2",
+                        "2 1",  "2 1"))
+                ,dynamicTest("Top Right Missing, other solution", () -> sudokuTest(
+                        "2 0",  "2 1",
+                        "1 2",  "1 2"))
+
+                ,dynamicTest("Bottom Right Missing", () -> sudokuTest(
+                        "1 2",  "1 2",
+                        "2 0",  "2 1"))
+                ,dynamicTest("Bottom Right Missing, other solution", () -> sudokuTest(
+                        "2 1",  "2 1",
+                        "1 0",  "1 2"))
+
+                ,dynamicTest("Bottom Left Missing", () -> sudokuTest(
+                        "1 2",  "1 2",
+                        "0 1",  "2 1"))
+                ,dynamicTest("Bottom Left Missing, other solution", () -> sudokuTest(
+                        "2 1",  "2 1",
+                        "0 2",  "1 2"))
+
+                 //Two cells missing
+                ,dynamicTest("Top Row Missing", () -> sudokuTest(
+                        "0 0",  "1 2",
+                        "2 1",  "2 1"))
+                ,dynamicTest("Top Row Missing, other solution", () -> sudokuTest(
+                        "0 0",  "2 1",
+                        "1 2",  "1 2"))
+
+                ,dynamicTest("Right Column Missing", () -> sudokuTest(
+                        "1 0",  "1 2",
+                        "2 0",  "2 1"))
+                ,dynamicTest("Right Column Missing, other solution", () -> sudokuTest(
+                        "2 0",  "2 1",
+                        "1 0",  "1 2"))
+
+                ,dynamicTest("Bottom Row Missing", () -> sudokuTest(
+                        "1 2",  "1 2",
+                        "0 0",  "2 1"))
+                ,dynamicTest("Bottom Row Missing, other solution", () -> sudokuTest(
+                        "2 1",  "2 1",
+                        "0 0",  "1 2"))
+
+                ,dynamicTest("Left Column Missing", () -> sudokuTest(
+                        "0 2",  "1 2",
+                        "0 1",  "2 1"))
+                ,dynamicTest("Left Column Missing, other solution", () -> sudokuTest(
+                        "0 1",  "2 1",
+                        "0 2",  "1 2"))
+
+                 //Three cells missing
+                ,dynamicTest("Top Left only present", () -> sudokuTest(
+                        "1 0",  "1 2",
+                        "0 0",  "2 1"))
+                ,dynamicTest("Top Left only present, other solution", () -> sudokuTest(
+                        "2 0",  "2 1",
+                        "0 0",  "1 2"))
+
+                ,dynamicTest("Top Right only present", () -> sudokuTest(
+                        "0 2",  "1 2",
+                        "0 0",  "2 1"))
+                ,dynamicTest("Top Right only present, other solution", () -> sudokuTest(
+                        "0 1",  "2 1",
+                        "0 0",  "1 2"))
+
+                ,dynamicTest("Bottom Right only present", () -> sudokuTest(
+                        "0 0",  "1 2",
+                        "0 1",  "2 1"))
+                ,dynamicTest("Bottom Right only present, other solution", () -> sudokuTest(
+                        "0 0",  "2 1",
+                        "0 2",  "1 2"))
+
+                ,dynamicTest("Bottom Left only present", () -> sudokuTest(
+                        "0 0",  "1 2",
+                        "2 0",  "2 1"))
+                ,dynamicTest("Bottom Left only present, other solution", () -> sudokuTest(
+                        "0 0",  "2 1",
+                        "1 0",  "1 2"))
                 );
     }
 
