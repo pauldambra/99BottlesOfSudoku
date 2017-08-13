71e514307c3b6b75b93e436cb9d058c700e57501
pauldambra <pauldambra@gmail.com>
2017-08-13 16:13:26 +0100

    started adding concepts to the code

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -1,21 +1,43 @@
 package sudoku;
 
+import java.util.Arrays;
+
+// if a cell is not set
+// then check if its neighbours are 2
+// * if it is set the cell to 1 and recurse
+// * if it is not
+// * * then check if its neighbours are 1
+// * * if they are then set the cell to 2 and recurse
+// the values repeat... the concepts repeat...
+// the concepts are missing from the code entirely
 public class Sudoku {
+
+    private boolean isSet(int[][] puzzle, int row, int column) {  return puzzle[row][column] != 0;  }
+
+    private int[] neighbours(int[][] puzzle, int row, int column) {
+        return new int[] { puzzle[1-row][column], puzzle[row][1-column] };
+    }
+
+    private boolean neighboursAre(int value, int[][] puzzle, int row, int column) {
+        int[] neighbours = neighbours(puzzle, row, column);
+        return Arrays.stream(neighbours).anyMatch(n -> n == value);
+    }
 
     public int[][] solve(int[][] puzzle) {
 
-        if (puzzle[1][1] == 0) {
-            if (puzzle[1][0] == 2 || puzzle[0][1] == 2) {
+        if (!isSet(puzzle, 1, 1)) {
+            if (neighboursAre(2, puzzle, 1, 1)) {
                 puzzle[1][1] = 1;
                 return solve(puzzle);
             }
-            if (puzzle[1][0] == 1 || puzzle[0][1] == 1){
+
+            if (neighboursAre(1, puzzle, 1, 1)){
                 puzzle[1][1] = 2;
                 return solve(puzzle);
             }
         }
 
-        if (puzzle[1][0] == 0) {
+        if (!isSet(puzzle, 1, 0)) {
             if (puzzle[1][1] == 2 || puzzle[0][0] == 2) {
                 puzzle[1][0] = 1;
                 return solve(puzzle);
@@ -26,7 +48,7 @@ public class Sudoku {
             }
         }
 
-        if (puzzle[0][1] == 0) {
+        if (!isSet(puzzle, 0, 1)) {
             if (puzzle[0][0] == 2 || puzzle[1][1] == 2) {
                 puzzle[0][1] = 1;
                 return solve(puzzle);
@@ -37,7 +59,7 @@ public class Sudoku {
             }
         }
 
-        if (puzzle[0][0] == 0) {
+        if (!isSet(puzzle, 0, 0)) {
             if (puzzle[0][1] == 2 || puzzle[1][0] == 2) {
                 puzzle[0][0] = 1;
                 return solve(puzzle);
